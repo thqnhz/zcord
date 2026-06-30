a16014fa34b98f1f3c675ac66a1a5bbe41851103
ThanhZ <89237062+thqnhz@users.noreply.github.com>
2026-06-30 14:55:08 +0700

    chore: easy __version__ string dedup from pyproject.toml

--- zcord/__init__.py
+++ zcord/__init__.py
@@ -1,9 +1,11 @@
 """Zcord - Functional programming discord API wrapper"""
 
+from importlib.metadata import version
+
 from .bot import Bot
 from .types import Channel, ChannelType, Message, Role, Snowflake, User
 
-__version__ = "2026.0.1.dev5"
+__version__ = version("zcord")
 
 __all__ = [
     "Bot",
